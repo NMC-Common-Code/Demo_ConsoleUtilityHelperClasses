5d2a6faebf11f869aaa17841a8abc842e88c77bf
John Velis <john@johnvelis.com>
2016-08-07 09:20:41 -0400

    committ 920

---
+++ Demo_ConsoleUtilityHelperClasses/Views/ConsoleViewSettings.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_ConsoleUtilityHelperClasses
+{
+    public static class ConsoleViewSettings
+    {
+        //
+        // window position
+        //
+        public const int WINDOw_TOP = 100;
+        public const int WINDOW_LEFT = 100;
+
+        //
+        // window size
+        //
+        public const int WINDOW_WIDTH = 79;
+        public const int WINDOW_HEIGHT = 40;
+
+        //
+        // horizontal and vertical margins in console window for display
+        //
+        public const int DISPLAY_HORIZONTAL_MARGIN = 3;
+        public const int DISPALY_VERITCAL_MARGIN = 1;
+    }
+}
